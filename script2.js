1c01fbd3b43c27fab96ebf51ef2058f84e9accd6
Sasha Johnson <johnson.sashamarie@gmail.com>
2020-09-05 13:01:42 +0800

    Finished Question 2

--- script2.js
+++ script2.js
@@ -1,30 +1,30 @@
-function addOrange() {
-    addItemToCart("Orange", 1.60)
-    console.log("Orange", 1.60)
-}
+// function addOrange(name, price) {
+//     addItemToCart("Orange", 1.60)
+//     console.log("Orange", 1.60)
+// }
 
-function addMango() {
-    addItemToCart("Mango", 2.30)
-    console.log("Mango", 2.30)
-}
+// // function addMango() {
+//     addItemToCart("Mango", 2.30)
+//     console.log("Mango", 2.30)
+// }
 
-function addStrawberry() {
-    addItemToCart("Strawberry", 1.40)
-    console.log("Strawberry", 1.40)
-}
+// function addStrawberry() {
+//     addItemToCart("Strawberry", 1.40)
+//     console.log("Strawberry", 1.40)
+// }
 
-function addPassionfruit() {
-    addItemToCart("Passionfruit", 3.40)
-    console.log("Passionfruit", 3.40)
-}
+// function addPassionfruit() {
+//     addItemToCart("Passionfruit", 3.40)
+//     console.log("Passionfruit", 3.40)
+// }
 
-function addPeach() {
-    addItemToCart("Peach", 1.70)
-    console.log("Peach", 1.70)
-}
+// function addPeach() {
+//     addItemToCart("Peach", 1.70)
+//     console.log("Peach", 1.70)
+// }
 
 function addItemToCart(Item, Price) {
-    let cart = document.getElementById("ShoppingCart")
+    let cart = document.getElementById("ShoppingCart");
     var node = document.createElement("LI");                 // Create a <li> node
     var textnode = document.createTextNode(`${Item} $${Price}`);         // Create a text node
     node.appendChild(textnode);                              // Append the text to <li>
