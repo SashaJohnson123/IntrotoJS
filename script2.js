31fd5013122768216bd39af4f2522c7e85fb104b
Sasha Johnson <johnson.sashamarie@gmail.com>
2020-09-05 11:53:41 +0800

    Question2

--- script2.js
+++ script2.js
@@ -1,6 +1,28 @@
-function addItemInCart() {
-    let item = document.getElementByClass("List").innerHTML;
-    console.log("item", item)
-    let add = parseInt(number) + 1;
-    document.getElementByClass("List").innerHTML = add
+function addOrange() {
+    addItemToCart("Orange", 1.60)
+}
+
+function addMango() {
+    addItemToCart("Mango", 2.30)
+}
+
+function addStrawberry() {
+    addItemToCart("Strawberry", 1.40)
+}
+
+function addPassionfruit() {
+    addItemToCart("Passionfruit")
+}
+
+function addPeach() {
+    addItemToCart("Peach")
+}
+
+
+function addItemToCart(Item, Price) {
+    let cart = document.getElementById("ShoppingCart")
+    var node = document.createElement("LI");                 // Create a <li> node
+    var textnode = document.createTextNode(`${Item} $${Price}`);         // Create a text node
+    node.appendChild(textnode);                              // Append the text to <li>
+    cart.appendChild(node);
 }
